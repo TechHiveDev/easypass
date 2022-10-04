1c7907186459226fedd2d10580a37680102774db
Mario <41785435+MarioMonir@users.noreply.github.com>
2022-10-04 11:37:11 +0200

    example entity

---
+++ server/src/entities/_example/_exampl.service.js
@@ -0,0 +1,41 @@
+/**
+ *  Entity Service
+ * ------------------------------------
+ * this module aim to carry custom bussniess logic
+ * for entity if entity not just CRUD
+ *
+ * you can add here any bussiness logic you need
+ * and will be calls from entity controller
+ *
+ * Entity
+ * router => controller => service => controller
+ *
+ */
+
+// ------------------------------------------------------------
+
+/**
+ *  you can call your orm here ( prisma ) and
+ *  return what you want from database
+ *
+ */
+
+ import { PrismaClient } from "@prisma/client";
+ const prisma = new PrismaClient();gg
+// ------------------------------------------------------------
+
+const exampleService = {
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+  
+    create: async (body) => prisma.user.create({ ...body }),
+  
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+  
+    mario: () => "mario example for custom route ",
+  
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+  };
+  
+  // ------------------------------------------------------------------
+  
+  e
