587fe46638e89844ccfd1ee03a0b712a5a99000b
Jan <jankaram2020@gmail.com>
2022-10-16 18:22:45 +0200

    add autocomplete with selection instead of normal select when choosing user or compound in all screens. fixed old image and new image now only one page when editing entities with photos. added full text search in compounds and users. filter with compound or/and user in properties, invitation and scan. filter with compound in announcement. translated user type field in scan and invitation. replace invitation id with "-" when user is resident. all reference fields now link to show page of entity instead of edit. added activate user from list view. replace users label in compound with properties.

--- client/src/entities/user/list.user.jsx
+++ client/src/entities/user/list.user.jsx
@@ -7,30 +7,76 @@ import {
   ShowButton,
   EditButton,
   DeleteButton,
-  ImageField,
+  SearchInput,
+  useTranslate,
+  WrapperField,
+  useRecordContext,
+  useLocaleState,
+  useDataProvider,
+  useRefresh,
 } from "react-admin";
 import Actions from "../../reactAdmin/components/Actions";
-
+import UserType from "../../components/UserType";
+import { useMutation } from "react-query";
+import CircularProgress from "@mui/material/CircularProgress";
+import Button from "@mui/material/Button";
+import AddTaskIcon from "@mui/icons-material/AddTask";
 // ------------------------------------------------
-
+const userFilters = [<SearchInput source="q" alwaysOn />];
+const ActiveButton = () => {
+  const refresh = useRefresh();
+  const [locale] = useLocaleState();
+  const { active } = useRecordContext();
+  const record = useRecordContext();
+  const dataProvider = useDataProvider();
+  const { mutateAsync, isLoading } = useMutation(
+    ["user", "update", { id: record.id }],
+    () =>
+      dataProvider.update("user", {
+        id: record.id,
+        data: { ...record, active: true },
+      })
+  );
+  return (
+    <Button
+      sx={{
+        opacity: active ? 0 : 1,
+        pointerEvents: active ? "none" : null,
+      }}
+      startIcon={<AddTaskIcon />}
+      disabled={isLoading}
+      onClick={async () => {
+        const res = await mutateAsync({ ...record, active: true });
+        if (res?.data?.id) {
+          refresh();
+        }
+      }}
+    >
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        ` ${locale === "ar" ? "فعل" : "activate"} `
+      )}
+    </Button>
+  );
+};
 export default function ListUser(props) {
   return (
-    <List>
+    <List filters={userFilters}>
       <Datagrid>
         <NumberField variant="outlined" source="id" />
         <TextField variant="outlined" source="email" />
         <TextField variant="outlined" source="name" />
-        {/*<TextField variant="outlined" source="password" />*/}
-        {/*<TextField variant="outlined" source="photoUrl" />*/}
-        {/*<ImageField source="photoUrl" title="photo" />*/}
         <TextField variant="outlined" source="phone" />
         <BooleanField variant="outlined" source="active" />
-        <TextField variant="outlined" source="type" />
-
+        <WrapperField label={"type"}>
+          <UserType />
+        </WrapperField>
         <Actions label="">
           <ShowButton label="ra.action.show" />
           <EditButton label="ra.action.edit" />
           <DeleteButton label="ra.action.delete" />
+          <ActiveButton />
         </Actions>
       </Datagrid>
     </List>
