4528c4592a345ee32f9ee69114575b1388bab18f
sergi-ss <sergi@thepalace.dev>
2022-10-19 09:56:01 +0200

    add userCompoundId to request

--- server/prisma/migrations/20221019075358_/migration.sql
+++ server/prisma/migrations/20221019075358_/migration.sql
@@ -67,6 +67,7 @@ CREATE TABLE `Scan` (
     `invitationId` INTEGER NULL,
     `deviceId` INTEGER NULL,
     `success` BOOLEAN NOT NULL,
+    `compoundId` INTEGER NOT NULL,
     `userId` INTEGER NOT NULL,
     `type` ENUM('Resident', 'Visitor', 'Delivery') NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -87,6 +88,53 @@ CREATE TABLE `Device` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Announcement` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
+    `photoUrl` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `compoundId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Facility` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `price` DOUBLE NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `photoUrl` VARCHAR(191) NULL,
+    `compoundId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Request` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `availableDateFrom` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `availableDateTo` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `status` ENUM('Pending', 'Refused', 'InProgress', 'Completed') NOT NULL DEFAULT 'Pending',
+    `type` ENUM('Facility', 'Issue') NOT NULL,
+    `facilityId` INTEGER NULL,
+    `requestNote` VARCHAR(191) NULL,
+    `respondNote` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `userCompoundId` INTEGER NOT NULL,
+    `compoundId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `UserCompound` ADD CONSTRAINT `UserCompound_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -100,7 +148,10 @@ ALTER TABLE `Invitation` ADD CONSTRAINT `Invitation_userId_fkey` FOREIGN KEY (`u
 ALTER TABLE `Invitation` ADD CONSTRAINT `Invitation_compoundId_fkey` FOREIGN KEY (`compoundId`) REFERENCES `Compound`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Scan` ADD CONSTRAINT `Scan_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Scan` ADD CONSTRAINT `Scan_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Scan` ADD CONSTRAINT `Scan_compoundId_fkey` FOREIGN KEY (`compoundId`) REFERENCES `Compound`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Scan` ADD CONSTRAINT `Scan_invitationId_fkey` FOREIGN KEY (`invitationId`) REFERENCES `Invitation`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
@@ -110,3 +161,24 @@ ALTER TABLE `Scan` ADD CONSTRAINT `Scan_deviceId_fkey` FOREIGN KEY (`deviceId`) 
 
 -- AddForeignKey
 ALTER TABLE `Device` ADD CONSTRAINT `Device_compoundId_fkey` FOREIGN KEY (`compoundId`) REFERENCES `Compound`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Announcement` ADD CONSTRAINT `Announcement_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Announcement` ADD CONSTRAINT `Announcement_compoundId_fkey` FOREIGN KEY (`compoundId`) REFERENCES `Compound`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Facility` ADD CONSTRAINT `Facility_compoundId_fkey` FOREIGN KEY (`compoundId`) REFERENCES `Compound`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Request` ADD CONSTRAINT `Request_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Request` ADD CONSTRAINT `Request_compoundId_fkey` FOREIGN KEY (`compoundId`) REFERENCES `Compound`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Request` ADD CONSTRAINT `Request_userCompoundId_fkey` FOREIGN KEY (`userCompoundId`) REFERENCES `UserCompound`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Request` ADD CONSTRAINT `Request_facilityId_fkey` FOREIGN KEY (`facilityId`) REFERENCES `Facility`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
