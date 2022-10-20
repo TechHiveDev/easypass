36a1d2f8fc090b99587ce755d92cf80630b0000b
sergi-ss <sergi@thepalace.dev>
2022-10-20 16:24:31 +0200

    add Discover entity

---
+++ server/src/entities/discover/discover.service.js
@@ -0,0 +1,39 @@
+/**
+ *  Entity Service
+ * ------------------------------------
+ * this module aim to carry custom bussniess logic
+ * for entity if entity not just CRUD
+ *
+ * you can add here any bussiness logic you need
+ * and will be calls from entity controller
+ *
+ * Entity
+ * router => controller => service => controller
+ *
+ */
+
+// ------------------------------------------------------------
+
+/**
+ *  you can call your orm here ( prisma ) and
+ *  return what you want from database
+ *
+ */
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// ------------------------------------------------------------
+
+export const getDiscoverByCompound = async (compoundId) => {
+  return await prisma.discover.findMany({ where: { compoundId } });
+};
+
+// ------------------------------------------------------------------
+
+export const createDiscover = async (adminId, data) => {
+  return await prisma.discover.create({
+    data: { ...data, userId: adminId },
+  });
+};
+// ------------------------------------------------------------------
