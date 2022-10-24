7bced799db23716790b2cee007c7cad58530a95f
Jan <jankaram2020@gmail.com>
2022-10-24 10:37:35 +0200

    change splash screen

--- mobile/src/Navigators/App.navigator.jsx
+++ mobile/src/Navigators/App.navigator.jsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import UnAuthorizedNavigator from "./UnAuthorized.navigator";
-import LoadingErrorEmpty from "../Components/GenericScreens/LoadingErrorEmpty.screen";
 import { useAuthMe } from "../Utils/auth.hook";
 import DrawerNavigator from "./Drawer.navigator";
+import * as SplashScreen from "expo-splash-screen";
 
 // ==============================================================
+SplashScreen.preventAutoHideAsync();
 
 export default function AppNavigator() {
   const { authMe, isAuthenticated, loading } = useAuthMe();
@@ -14,12 +15,17 @@ export default function AppNavigator() {
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
   useEffect(() => {
-    authMe();
+    authMe().then(() =>
+      setTimeout(() => {
+        SplashScreen.hideAsync();
+        // half a second so that every thing is done
+      }, 500)
+    );
   }, []);
 
-  // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-  if (loading) return <LoadingErrorEmpty isLoading={loading} />;
+  if (loading) {
+    return null;
+  }
 
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
