b1d55b7f07c0b0e673be11dc9ade6609e087774d
Jan <jankaram2020@gmail.com>
2022-12-13 14:50:29 +0200

    put image in header in homepage

--- mobile/src/Components/MyStatusBar.jsx
+++ mobile/src/Components/MyStatusBar.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import {Image, StyleSheet} from "react-native";
 import { Appbar } from "react-native-paper";
 import theme from "../Theme/paper.theme";
 import {
@@ -6,7 +6,9 @@ import {
   widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 
-const hiddenStatusBar = ["login", "Home", "AllFacilityCategories"];
+// const hiddenStatusBar = ["login", "Home", "AllFacilityCategories"];
+const imageStatusBar = ["Home"];
+const hiddenStatusBar = ["login", "AllFacilityCategories"];
 const allowBackList = [
   "SingleDiscoveryCategory",
   "DiscoverItem",
@@ -28,6 +30,17 @@ export default function MyStatusBar({
     : name;
   const allowBack = params?.allowBack || allowBackList.includes(name);
   if (hiddenStatusBar.includes(name)) return <></>;
+  if (imageStatusBar.includes(name)) {
+    return (
+        <Appbar.Header style={styles.imageHeader} statusBarHeight={0}>
+          <Image
+              source={require("../../assets/splash.png")}
+              resizeMode={"contain"}
+              style={styles.image}
+          />
+        </Appbar.Header>
+    );
+  }
   if (allowBack) {
     return (
       <Appbar.Header style={styles.header} statusBarHeight={0}>
@@ -64,9 +77,19 @@ const styles = StyleSheet.create({
     borderColor: theme.colors.grey,
     borderBottomWidth: hp(0.1),
   },
+  // image: {
+  //   width: wp(35),
+  //   height: hp(5),
+  // },
   image: {
-    width: wp(35),
-    height: hp(5),
+    width: wp(50),
+    height: hp(6),
+    marginHorizontal: wp(25),
+  },
+  imageHeader: {
+    backgroundColor: theme.colors.white,
+    elevation: 0,
+    borderWidth: 0,
   },
   content: {
     fontSize: 22,
